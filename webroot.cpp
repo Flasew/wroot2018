eabe3e11081849663c5f0a601d31236de1bd0978
Flasew <af141_l@icloud.com>
2018-01-27 12:44:40 -0800

    board, player

--- webroot.cpp
+++ webroot.cpp
@@ -20,21 +20,29 @@ int main()
         int helperBots; // your number of charges left to deploy helper bots
         cin >> helperBots; cin.ignore();
 
-        vector<pos> positions;
+        vector<pos> positions(playerCount);
 
         for (int i = 0; i < playerCount; i++) {
             int x; // your bot's coordinates on the grid (0,0) is top-left
             int y;
             cin >> x >> y; cin.ignore();
-            if (i == playerCount)   myPos = make_pair(x, y);
+            positions[i] = make_pair(x, y);
         }
 
         int removalCount; // the amount walls removed this turn by helper bots
         cin >> removalCount; cin.ignore();
+
+        bool board[15][30];
+
+        for (int r = 0; r < 15; r++)
+            for (int c = 0; c < 30; c++)
+                board[r][c] = 1;
+
         for (int i = 0; i < removalCount; i++) {
             int removeX; // the coordinates of a wall removed this turn
             int removeY;
             cin >> removeX >> removeY; cin.ignore();
+            board[removeX][removeY] =  0;
         }
 
         // Write an action using cout. DON'T FORGET THE "<< endl"
